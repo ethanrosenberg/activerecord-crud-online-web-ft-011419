e2c77013d6c328cb10abf53e48fd2e5b9e07a021
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-27 23:17:05 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block(args = {})
   # title == "Home Alone"
   # release_date == 1990
   Movie.create do |m|
-    if args.size = 0
+    if args.size == 0
       m.title = "Home Alone"
       m.release_date = 1990
     else

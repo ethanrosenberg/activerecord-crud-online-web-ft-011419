5b9d7260b7b61fdc6f695e5929d4336be1e0dc67
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-28 00:51:41 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -64,8 +64,6 @@ def can_find_by_multiple_attributes
   Movie.find_by(title: "Title")
   Movie.find_by(release_date: 2000)
   Movie.find_by(director: "Me")
-
-  __
 end
 
 def can_find_using_where_clause_and_be_sorted

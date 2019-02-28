93ba7b914cede2933ffe16861ca5f41b42ed48fc
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-28 01:03:42 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -103,6 +103,7 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   found_movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
+  found_movie.destroy
   __
   __
 end
